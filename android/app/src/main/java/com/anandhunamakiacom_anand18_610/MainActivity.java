ffb0ecabb1b3b31f2701feecfab424268ba275a1
Saurav <sp.gharti@gmail.com>
2018-11-24 17:52:34 +0000

    initial commit

---
+++ android/app/src/main/java/com/anandhunamakiacom_anand18_610/MainActivity.java
@@ -0,0 +1,15 @@
+package com.anandhunamakiacom_anand18_610;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "anandhunamakiacom_anand18_610";
+    }
+}
